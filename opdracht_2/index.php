d5917ed48b90578265062183106e06f93d561fb1
Dinandvanwieringen <9020564@student.zadkine.nl>
2022-12-05 20:28:28 +0100

    /

---
+++ opdracht_2/index.php
@@ -0,0 +1,12 @@
+<?php
+    // zo kan je een comment toevoegen
+
+    # zo kan je een comment toevoegen
+
+    /* zo kan je een comment toevoegen
+       over meerdere regels */
+
+    // Parse error: syntax error, unexpected end of file in php is een error die je krijgt als je brackets te dicht bij de open of close van php doet
+
+        echo "hello world";
+?>
